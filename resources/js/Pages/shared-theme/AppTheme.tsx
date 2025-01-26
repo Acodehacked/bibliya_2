dfae80cca0c3875a06544815864f5523f4a096d1
Abin Antony <abina5448@gmail.com>
2025-01-26 18:13:46 +0530

    app theme

--- resources/js/Pages/shared-theme/AppTheme.tsx
+++ resources/js/Pages/shared-theme/AppTheme.tsx
@@ -32,7 +32,6 @@ export default function AppTheme(props: AppThemeProps) {
           colorSchemeSelector: 'data-mui-color-scheme',
           cssVarPrefix: 'template',
         },
-        colorSchemes, // Recently added in v6 for building light & dark mode app, see https://mui.com/material-ui/customization/palette/#color-schemes
         typography,
         shadows,
         shape,
